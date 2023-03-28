2e5082a0d91bdbff884f2ddb45f84d505f3eb0bf
Vinh Nguyen <vinh28062002@gmail.com>
2023-03-27 22:09:42 -0400

    final changes to password

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(30), nullable=False)
+    password_hash = db.Column(db.String(30), nullable=False)
     products = db.relationship('Product', backref='seller', lazy='select', cascade='all, delete-orphan')
 
     def __repr__(self):
